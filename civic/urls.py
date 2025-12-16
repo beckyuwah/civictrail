f692c12bd2eb590df72adc0be8669feb67f62025
Becky Uwah <beckyuwah@Beckys-MacBook-Pro.local>
2025-12-16 18:02:06 +0000

    Resolve merge conflict

--- civic/urls.py
+++ civic/urls.py
@@ -20,12 +20,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('civictrail.urls')),
-<<<<<<< HEAD
-    path('users/', include('users.urls')),  
-    
-=======
     path("users/", include("users.urls")),
     path("projects/", include("projects.urls")),
 
->>>>>>> feature/projects-module
 ]
