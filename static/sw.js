932e74173ebd8c3909e5d0493c7f108403735b0e
Becky Uwah <beckyuwah@Beckys-MacBook-Pro.local>
2025-12-23 08:15:07 +0000

    create service worker

---
+++ static/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "civictrail-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/login/",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
